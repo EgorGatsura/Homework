906301a84baa16911879ed3acc27bf22efb5b51b
EgorGatsura <egorhatsura@gmail.com>
2022-08-07 17:32:02 +0300

    Homework 3
    Removed the extra lines of code

--- Homework/Homework 3/Program.cs
+++ Homework/Homework 3/Program.cs
@@ -7,40 +7,24 @@ if (a >= 1 && a <= 12)
     switch (a)
     {
         case 1:
-            Console.WriteLine("Winter");
-            break;
-        case 2:
+            case 2:
+                case 12:
             Console.WriteLine("Winter");
             break;
         case 3:
-            Console.WriteLine("Spring");
-            break;
-        case 4:
-            Console.WriteLine("Spring");
-            break;
-        case 5:
+            case 4:
+                case 5:
             Console.WriteLine("Spring");
             break;
         case 6:
-            Console.WriteLine("Summer");
-            break;
-        case 7:
-            Console.WriteLine("Summer");
-            break;
-        case 8:
+            case 7:
+                case 8:
             Console.WriteLine("Summer");
             break;
         case 9:
+            case 10:
+                case 11:
             Console.WriteLine("Autumn");
-            break;
-        case 10:
-            Console.WriteLine("Autumn");
-            break;
-        case 11:
-            Console.WriteLine("Autumn");
-            break;
-        case 12:
-            Console.WriteLine("Winter");
             break;
     }
 }
