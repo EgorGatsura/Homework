0f3cb090bee62c9cd023b6c1b8520eab9b377aee
EgorGatsura <egorhatsura@gmail.com>
2022-08-07 17:50:55 +0300

    Homework 6
    "or" it is ||

--- Homework/Homework 6/Program.cs
+++ Homework/Homework 6/Program.cs
@@ -3,13 +3,13 @@ Console.WriteLine("Enter the number you want to check");
 ulong a = Convert.ToUInt64(Console.ReadLine());
 if (a >= 2)
 {
-    if (a == 2 | a == 3 | a == 5 | a == 7)
+    if (a == 2 || a == 3 || a == 5 || a == 7)
     {
         Console.WriteLine("This number is prime");
         return;
     }
 
-    if (a % 2 == 0 | a % 3 == 0 | a % 5 == 0 | a % 7 == 0)
+    if (a % 2 == 0 || a % 3 == 0 || a % 5 == 0 || a % 7 == 0)
     {
         Console.WriteLine("This number is not prime");
     }
