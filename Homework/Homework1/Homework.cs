86a678e28cc6c4d402e9179818b5140ccd6813bb
EgorGatsura <egorhatsura@gmail.com>
2022-08-12 12:46:05 +0300

    Homework1
    unfinished Method

---
+++ Homework/Homework1/Homework.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework1;
+
+public class Homework
+{
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        return a + b >= c && a + c >= b && c + b >= a && a != 0 && b != 0 && c != 0;
+    }
+}
---
+++ Homework/Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Homework1;
+
+Console.WriteLine("Enter the length of the first side of the triangle");
+int a;
+while (!int.TryParse(Console.ReadLine(), out a))
+{
+    Console.WriteLine("Enter the length");
+}
+
+Console.WriteLine("Enter the length of the second side of the triangle");
+int b;
+while (!int.TryParse(Console.ReadLine(), out b))
+{
+    Console.WriteLine("Enter the length");
+}
+
+Console.WriteLine("Enter the length of the third side of the triangle");
+int c;
+while (!int.TryParse(Console.ReadLine(), out c))
+    Console.WriteLine("Enter the length");
+
+Console.WriteLine($"Length of the first side {a}  Length of the second side {b}  Length of the third side {c}");
+Homework.IsTriangle(a, b, c);
+bool d = 
+Console.ReadLine();
