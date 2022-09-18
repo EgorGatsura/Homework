3ba2cfa9f76da267cc631338f8764c78c82842eb
EgorGatsura <egorhatsura@gmail.com>
2022-09-18 16:56:25 +0300

    Homework3
    Added input verification

--- src/Homework2/Program.cs
+++ src/Homework2/Program.cs
@@ -2,6 +2,11 @@
 
 Console.WriteLine("What grade did you get?");
 int a = Convert.ToInt32(Console.ReadLine());
+while (!int.TryParse(Console.ReadLine(), out a))
+{
+    Console.WriteLine("Enter the grade you received");
+}
+
 Grade result = Methods.Convert(a);
 Console.WriteLine(result.ToString());
 Console.ReadLine();
--- src/Homework3/Program.cs
+++ src/Homework3/Program.cs
@@ -3,7 +3,12 @@
 Console.WriteLine("Choose the name of the month");
 Console.WriteLine(
     "1 [January] 2 [February] 3 [March] 4 [April] 5 [May] 6 [June] 7 [July] 8 [August] 9 [September] 10 [October] 11 [November] 12 [December]");
-int a = Convert.ToInt32(Console.ReadLine());
+int a;
+while (!int.TryParse(Console.ReadLine(), out a))
+{
+    Console.WriteLine("Choose an option according to the suggested");
+}
+
 if (a >= 1 && a <= 12)
 {
     EnumSeason result = Seasons.MethodSeason(a);
